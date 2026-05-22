aac407b6cfb7c80aa22b4ac6d33182c0faeaf379
MoeinAfshari <afsharimoein79@gmail.com>
2026-05-22 17:54:23 +0330

    7 edition: 7.py reformatted with black tool

--- 7.py
+++ 7.py
@@ -1,21 +1,25 @@
 # problem 7
-'''
+"""
 What is the 10001st prime number?
-'''
+"""
+
 import math
+
+
 # checks a number is prime or not
 def is_prime(n):
     if n <= 1:
         return False
     if n == 2 or n == 3:
         return True
-    for i in range(2, int(math.sqrt(n))+1):
+    for i in range(2, int(math.sqrt(n)) + 1):
         if n % i == 0:
             return False
     return True
 
+
 # main function
-def find_prime(n): # n is number of prime numbers
+def find_prime(n):  # n is number of prime numbers
     primes = []
     num = 1
     while len(primes) < n:
@@ -23,6 +27,7 @@ def find_prime(n): # n is number of prime numbers
             primes.append(num)
         num += 1
     return primes[-1]
+
 
 # final answer
 if __name__ == "__main__":
